0fa7479f57d3ac1e23ea97a9b73a9487c0c9db5b
Neil Robichaud <neil.robichaud@hotmail.com>
2016-02-20 14:16:31 -0500

    I added vyomes code

--- src/ColorfulCircles.java
+++ src/ColorfulCircles.java
@@ -20,16 +20,32 @@ import javafx.stage.Stage;
 // make another one where it just changes boolean values to true and false and will spit out the errors when  validate button is pushed ::::: in progress
 public class ColorfulCircles extends Application {
 	
+	private boolean PlayerTurn=true;
 	private int redCount = 1;
 	private int blueCount = 1;
 	private boolean duplicate = false;
 	private boolean numPieces = false;
+	private boolean rmPiece=false;
+	private boolean NewGame=false;
 	private boolean isSbox = false;
 	private String lastcolor = "white";
 	private String pickedcolor = "black";
+	private String [][][]xycheck=new String[2][4][3];//[0][][] for x [1][][] for y
+	
+	public void initxycheck(){//for various purposes, get rid of nulls when initialized
+		for(int i=0;i<xycheck.length;i++){
+			for(int j=0;j<xycheck[i].length;j++){
+				for(int k=0;k<xycheck[i][j].length;k++){
+					xycheck[i][j][k]="noColor";
+				}
+			}
+		}
+	}
+			
+	
 	@Override
 	public void start(Stage primaryStage) {
-		
+		initxycheck();
 		Scene scene1 = new Scene(shellmaker(2),800,600,Color.BEIGE);
 		primaryStage.setScene(scene1);	
 		primaryStage.setTitle("6 mens morris");
@@ -47,7 +63,8 @@ public class ColorfulCircles extends Application {
 			Circle circle = new Circle (x, y, sizenode, nodecolor);
 			setTranslateX(x);
 			setTranslateY(y);
-			getChildren().addAll(circle);    		
+			getChildren().addAll(circle);    
+			
 			setOnMouseClicked(new EventHandler<MouseEvent>()    		
 			{
 
@@ -78,6 +95,41 @@ public class ColorfulCircles extends Application {
 						}
 						
 						
+					}					
+					else if(rmPiece==true){
+						if(pickedcolor.equals("removered")){
+							if(circle.getFill()==Color.RED){
+								circle.setFill(Color.BLACK);
+								rmPiece=false;
+								StartTurn();
+								removeFromArray(circle.getCenterX(),circle.getCenterY());
+							}
+							else{
+								Alert alert = new Alert(AlertType.INFORMATION);
+								alert.setTitle("Information Dialog");
+								alert.setHeaderText("Invalid move");
+								alert.setContentText("You can only remove red discs");
+								alert.showAndWait();
+							}
+							
+						}
+						
+						else{
+							if(circle.getFill()==Color.BLUE){
+								circle.setFill(Color.BLACK);
+								rmPiece=false;
+								StartTurn();
+								removeFromArray(circle.getCenterX(),circle.getCenterY());
+							}
+							else{
+								Alert alert = new Alert(AlertType.INFORMATION);
+								alert.setTitle("Information Dialog");
+								alert.setHeaderText("Invalid move");
+								alert.setContentText("You can only remove blue discs");
+								alert.showAndWait();
+							}
+							
+						}
 					}
 					else {
 					if (!(circle.getFill().toString().equals("0x000000ff"))){
@@ -87,6 +139,13 @@ public class ColorfulCircles extends Application {
 						alert.setContentText("Please try again");
 						alert.showAndWait();
 
+					}
+					else if(redCount>6&&blueCount>6){
+						Alert alert = new Alert(AlertType.INFORMATION);
+						alert.setTitle("Information Dialog");
+						alert.setHeaderText("Phase 1 has ended");
+						alert.setContentText("Game will resume with Phase 2");
+						alert.showAndWait();
 					}
 					//check for placing more than 6 pieces in total 
 					else if (redCount >6 && pickedcolor == "red") {
@@ -105,6 +164,7 @@ public class ColorfulCircles extends Application {
 						alert.showAndWait();
 
 					}
+					
 					else if (pickedcolor == "red"){ 
 						if (lastcolor.equals("red")){												//for regular play, cannot place two of the same piece in a row
 							Alert alert = new Alert(AlertType.INFORMATION);
@@ -116,7 +176,31 @@ public class ColorfulCircles extends Application {
 						else{
 							circle.setFill(Color.RED);
 							redCount++;
+							
+							addToArrays(circle.getCenterX(),circle.getCenterY());
+							//System.out.println(circle.getCenterX()+" "+circle.getCenterY());
+							if(formedMill(circle.getCenterX(),circle.getCenterY())==true){
+								rmPiece=true;
+								pickedcolor="removeblue";
+								lastcolor = "red";
+								Alert alert = new Alert(AlertType.INFORMATION);
+								alert.setTitle("Red formed a mill");
+								alert.setHeaderText("You can now remove a blue disc");
+								alert.setContentText("Click on a blue disc to remove it.");
+								alert.showAndWait();
+							}
+							else{
+							StartTurn();
+							//System.out.println(pickedcolor);
 							lastcolor = "red";
+							if(redCount>6&&blueCount>6){
+								Alert alert = new Alert(AlertType.INFORMATION);
+								alert.setTitle("Information Dialog");
+								alert.setHeaderText("Phase 1 has ended");
+								alert.setContentText("Game will resume with Phase 2");
+								alert.showAndWait();
+							}
+							}
 						}
 					}
 					
@@ -131,7 +215,32 @@ public class ColorfulCircles extends Application {
 						else{
 							circle.setFill(Color.BLUE);
 							blueCount++;
+							addToArrays(circle.getCenterX(),circle.getCenterY());
+							//System.out.println(circle.getCenterX()+" "+circle.getCenterY());
+							if(formedMill(circle.getCenterX(),circle.getCenterY())==true){
+								rmPiece=true;
+								pickedcolor="removered";
+								lastcolor = "blue";
+								Alert alert = new Alert(AlertType.INFORMATION);
+								alert.setTitle("Blue formed a mill");
+								alert.setHeaderText("You can now remove a red disc");
+								alert.setContentText("Click on a red disc to remove it.");
+								alert.showAndWait();
+							}
+							else{
+								
+							
+							StartTurn();
+							//System.out.println(pickedcolor);
 							lastcolor = "blue";
+							if(redCount>6&&blueCount>6){
+								Alert alert = new Alert(AlertType.INFORMATION);
+								alert.setTitle("Information Dialog");
+								alert.setHeaderText("Phase 1 has ended");
+								alert.setContentText("Game will resume with Phase 2");
+								alert.showAndWait();
+							}
+							}
 						}
 					}										
 					
@@ -142,6 +251,237 @@ public class ColorfulCircles extends Application {
 				
 			});
 		}    	
+	}
+	public void addToArrays(double x,double y){//keeps track of color in each circle
+		if(x==280.0){
+			if(y==180.0){
+				xycheck[0][0][0]=pickedcolor;
+				xycheck[1][0][0]=pickedcolor;
+			}
+			if(y==300.0){
+				xycheck[0][0][1]=pickedcolor;
+			}
+			if(y==420.0){
+				xycheck[0][0][2]=pickedcolor;
+				xycheck[1][3][0]=pickedcolor;
+			}
+		}
+		else if(x==340.0){
+			if(y==240.0){
+				xycheck[0][1][0]=pickedcolor;
+				xycheck[1][1][0]=pickedcolor;
+			}
+			if(y==300.0){
+				xycheck[0][1][1]=pickedcolor;
+			}
+			if(y==360.0){
+				xycheck[0][1][2]=pickedcolor;
+				xycheck[1][2][0]=pickedcolor;
+			}
+		}
+		else if(x==400.0){
+			if(y==180.0){
+				xycheck[1][0][1]=pickedcolor;
+			}
+			if(y==240.0){
+				xycheck[1][1][1]=pickedcolor;
+			}
+			
+			if(y==360.0){
+				xycheck[1][2][1]=pickedcolor;
+			}
+			if(y==420.0){
+				xycheck[1][3][1]=pickedcolor;
+			}
+		}
+		else if(x==460.0){
+			
+			if(y==240.0){
+				xycheck[0][2][0]=pickedcolor;
+				xycheck[1][1][2]=pickedcolor;
+			}
+			if(y==300.0){
+				xycheck[0][2][1]=pickedcolor;
+			}
+			if(y==360.0){
+				xycheck[0][2][2]=pickedcolor;
+				xycheck[1][2][2]=pickedcolor;
+			}
+			
+		}
+		else if(x==520.0){
+			
+			if(y==180.0){
+				xycheck[0][3][0]=pickedcolor;
+				xycheck[1][0][2]=pickedcolor;
+			}
+			if(y==300.0){
+				xycheck[0][3][1]=pickedcolor;
+			}
+			if(y==420.0){
+				xycheck[0][3][2]=pickedcolor;
+				xycheck[1][3][2]=pickedcolor;
+			}
+			
+		}
+		
+	}
+	public void removeFromArray(double x,double y){
+		if(x==280.0){
+		if(y==180.0){
+			xycheck[0][0][0]="noColor";
+			xycheck[1][0][0]="noColor";
+		}
+		if(y==300.0){
+			xycheck[0][0][1]="noColor";
+		}
+		if(y==420.0){
+			xycheck[0][0][2]="noColor";
+			xycheck[1][3][0]="noColor";
+		}
+	}
+	else if(x==340.0){
+		if(y==240.0){
+			xycheck[0][1][0]="noColor";
+			xycheck[1][1][0]="noColor";
+		}
+		if(y==300.0){
+			xycheck[0][1][1]="noColor";
+		}
+		if(y==360.0){
+			xycheck[0][1][2]="noColor";
+			xycheck[1][2][0]="noColor";
+		}
+	}
+	else if(x==400.0){
+		if(y==180.0){
+			xycheck[1][0][1]="noColor";
+		}
+		if(y==240.0){
+			xycheck[1][1][1]="noColor";
+		}
+		
+		if(y==360.0){
+			xycheck[1][2][1]="noColor";
+		}
+		if(y==420.0){
+			xycheck[1][3][1]="noColor";
+		}
+	}
+	else if(x==460.0){
+		
+		if(y==240.0){
+			xycheck[0][2][0]="noColor";
+			xycheck[1][1][2]="noColor";
+		}
+		if(y==300.0){
+			xycheck[0][2][1]="noColor";
+		}
+		if(y==360.0){
+			xycheck[0][2][2]="noColor";
+			xycheck[1][2][2]="noColor";
+		}
+		
+	}
+	else if(x==520.0){
+		
+		if(y==180.0){
+			xycheck[0][3][0]="noColor";
+			xycheck[1][0][2]="noColor";
+		}
+		if(y==300.0){
+			xycheck[0][3][1]="noColor";
+		}
+		if(y==420.0){
+			xycheck[0][3][2]="noColor";
+			xycheck[1][3][2]="noColor";
+		}
+		
+	}
+		
+	}
+	public boolean formedMill(double x,double y){//check if mill was formed with the last click
+		boolean millfound=false;
+		
+		for(int i=0;i<xycheck.length;i++){
+			for(int j=0;j<xycheck[i].length;j++){
+				
+				if((xycheck[i][j][0].equals(xycheck[i][j][1]))&&(xycheck[i][j][1].equals(xycheck[i][j][2]))&&xycheck[i][j][0].equals(pickedcolor)){
+					if(x==280.0){
+						if(y==180.0){
+							if(j==0)millfound=true;
+							
+						}
+						if(y==300.0){
+							if(i==0&&j==0)millfound=true;
+							
+						}
+						if(y==420.0){
+							if((i==0&&j==0)||(i==1&&j==3))millfound=true;
+							
+						}
+					}
+					else if(x==340.0){
+						if(y==240.0){
+							if(j==1)millfound=true;
+						}
+						if(y==300.0){
+							if(i==0&&j==1)millfound=true;
+						}
+						if(y==360.0){
+							if((i==0&&j==1)||(i==1&&j==2))millfound=true;
+							
+						}
+					}
+					else if(x==400.0){
+						if(y==180.0){
+							if(i==1&&j==0)millfound=true;
+						}
+						if(y==240.0){
+							if(i==1&&j==1)millfound=true;
+						}
+						
+						if(y==360.0){
+							if(i==1&&j==2)millfound=true;
+						}
+						if(y==420.0){
+							if(i==1&&j==3)millfound=true;
+						}
+					}
+					else if(x==460.0){
+						
+						if(y==240.0){
+							if((i==0&&j==2)||(i==1&&j==1))millfound=true;
+						}
+						if(y==300.0){
+							if(i==0&&j==2)millfound=true;
+						}
+						if(y==360.0){
+							if((i==0&&j==2)||(i==1&&j==2))millfound=true;
+						}
+						
+					}
+					else if(x==520.0){
+						
+						if(y==180.0){
+							if((i==0&&j==3)||(i==1&&j==0))millfound=true;
+						}
+						if(y==300.0){
+							if(i==0&&j==3)millfound=true;
+						}
+						if(y==420.0){
+							if((i==0&&j==3)||(i==1&&j==3))millfound=true;
+						}
+						
+					}
+					
+				}
+				
+			}
+		}
+		
+		
+		return millfound;
 	}
 	// just for making the board and placing the lines
 	public Parent shellmaker(int shellnum){
@@ -192,29 +532,94 @@ public class ColorfulCircles extends Application {
 		redbutton.setOnAction(new EventHandler<ActionEvent>(){
 			@Override
 			public void handle(ActionEvent arg0){				
+				if(isSbox!=true){
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Information Dialog");
+					alert.setHeaderText("Not in Sandbox mode");
+					alert.setContentText("This button can only be used in sandbox mode.");
+					alert.showAndWait();
+				}
+				else{
 				pickedcolor = "red";	
-
+				}
 			}
 		});
 		nodes.getChildren().add(redbutton);
 
 		Button bluebutton = new Button("Place a blue piece");
-		bluebutton.setTranslateX(350);    	
+		 
+		bluebutton.setTranslateY(30);
 		bluebutton.setOnAction(new EventHandler<ActionEvent>(){
 			@Override
 			public void handle(ActionEvent arg0){				
+				if(isSbox!=true){
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Information Dialog");
+					alert.setHeaderText("Not in Sandbox mode");
+					alert.setContentText("This button can only be used in sandbox mode.");
+					alert.showAndWait();
+				}
+				else{
 				pickedcolor = "blue";	
+				}	
 
 			}
 		});
 
 		nodes.getChildren().add(bluebutton);
 		
+		
+		Button newgame = new Button("New Game");
+		newgame.setTranslateX(350);
+		
+		newgame.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent arg0){
+				pickedcolor="newgame";
+				NewGame=true;
+				isSbox=false;
+				newGame();
+				if(pickedcolor.equals("newgame")||NewGame==true){
+					if(PlayerTurn==true){
+						pickedcolor="blue";
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Information Dialog");
+					alert.setHeaderText("New Game has started.");
+					alert.setContentText("Blue will start the game.");
+					alert.showAndWait();
+					}
+					else{
+						pickedcolor="red";
+						Alert alert = new Alert(AlertType.INFORMATION);
+						alert.setTitle("Information Dialog");
+						alert.setHeaderText("New Game has started.");
+						alert.setContentText("Red will start the game.");
+						alert.showAndWait();
+					}
+					
+				}
+
+			}
+		});
+
+		nodes.getChildren().add(newgame);
+		
+		
+		
 		Button validButton = new Button("Validate");
 		validButton.setTranslateX(700);    	
 		validButton.setOnAction(new EventHandler<ActionEvent>(){
 			@Override
 			public void handle(ActionEvent arg0){
+				if(isSbox!=true){
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Information Dialog");
+					alert.setHeaderText("Not in Sandbox mode");
+					alert.setContentText("This button can only be used in sandbox mode.");
+					alert.showAndWait();
+					
+				}
+				else{
 				if (numPieces==true || duplicate ==true){
 					Alert alert = new Alert(AlertType.INFORMATION);
 					alert.setTitle("Information Dialog");
@@ -229,6 +634,7 @@ public class ColorfulCircles extends Application {
 					alert.setContentText("");
 					alert.showAndWait();
 				}	
+				}
 			}
 		});
 
@@ -241,6 +647,8 @@ public class ColorfulCircles extends Application {
 			@Override
 			public void handle(ActionEvent arg0){				
 				pickedcolor = "sBox";
+				isSbox=true;
+				NewGame=false;
 			}
 		});
 
@@ -249,7 +657,21 @@ public class ColorfulCircles extends Application {
 	}
 	
 	
-
+	public void newGame(){
+    	int randomnum=(int)(Math.random()*2);
+    	if(randomnum==0)PlayerTurn=true;//blue
+    	else PlayerTurn=false;//red
+    }
+	public void StartTurn(){
+    	if(PlayerTurn==true){
+    		PlayerTurn=false;
+    		pickedcolor="red";
+    	}
+    	else{
+    		PlayerTurn=true;
+    		pickedcolor="blue";
+    	}
+    }
 
 	public static void main(String[] args) {
 		launch(args);
